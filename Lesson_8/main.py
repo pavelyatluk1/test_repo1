2a157b920dfe147dd719cb0070259aa124dacc12
Yatluk Pavlo <pavelyatluk@gmail.com>
2022-11-13 13:24:33 +0200

    Add new function test_function_2()

--- Lesson_8/main.py
+++ Lesson_8/main.py
@@ -8,4 +8,9 @@ def test_function():
         import math
         s = math.pi * (r ** 2)
 r = float(input()) 
-print(test_function())       
+print(test_function())
+
+
+def test_function_2():
+    import math
+    l = 2 * math.pi * r
