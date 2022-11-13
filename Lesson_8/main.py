acaba72d45ddb7add959f7a47fad33bfbaa06e81
Yatluk Pavlo <pavelyatluk@gmail.com>
2022-11-13 14:08:49 +0200

    Add run "sqrt_1()" func

--- Lesson_8/main.py
+++ Lesson_8/main.py
@@ -1,10 +1,12 @@
 def sqrt_1():
     sq = a * b
     return sq
+print(sqrt_1())
     
     
 def test_function():
         import math
         s = math.pi * (r ** 2)
 r = float(input()) 
-print(test_function())       
+print(test_function())
+
