ba8c42dc4fefbfd54f4eafad95e1a9c162d5dd66
Yatluk Pavlo <pavelyatluk@gmail.com>
2022-11-15 08:05:26 +0200

    Run "sl()" func

--- Lesson_8/py_charm.py
+++ Lesson_8/py_charm.py
@@ -1,6 +1,6 @@
-def sl():
-    import math
-    l = 2 * math.pi * r
-    return l
-r = float(input())
-print(sl())
+def sqrt_2():
+    p = 2 * a + 2 * b
+    return p
+a = float(input())
+b = float(input())
+print(sqrt_2())
